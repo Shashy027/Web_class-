6d8db24638b23ffdf8200da1049ffba9725aecaa
Shashy027 <109581064+Shashy027@users.noreply.github.com>
2022-11-25 10:08:32 +0545

    Add files via upload

---
+++ even.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>even numbers</title>
+</head>
+<body>
+<?php
+for($even=0 ; $even<=100; $even+=2){
+	
+   	if ($even == 60){
+    	continue;
+        }
+  echo " $even ";
+        }
+        ?>
+</body>
+</html>
